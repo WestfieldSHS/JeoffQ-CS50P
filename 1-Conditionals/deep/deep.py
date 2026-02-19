7575107e22a527e6fccba4354e18f9275369f31d
jeoffquinoy <jeoff.quinoy426@education.nsw.gov.au>
2026-02-19 23:42:36 +0000

    PROBLEM1.1 COMPLETED

--- 1-Conditionals/deep/deep.py
+++ 1-Conditionals/deep/deep.py
@@ -1,3 +1,9 @@
-def main():
-    answer = input('Answer? ')
-    
+answer = input('answer? ')
+if answer == '42':
+    print('Yes!')
+elif answer == 'Forty two':
+    print('Yes!')
+elif answer == 'forty two':
+    print('Yes!')
+else:
+    print('No.')
